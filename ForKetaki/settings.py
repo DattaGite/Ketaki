22dff1f5a7fae50c02326bf5760cdf400bdc67bf
DattaGite <your-email@example.com>
2025-07-03 15:33:49 +0530

    second commit for Ketaki project

--- ForKetaki/settings.py
+++ ForKetaki/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ys#o!_n(ftp^@-h#nr2p60k+l!njbp(0(7nhqve4r$fnmm=6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.29.58', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.29.58', '*','localhost', '127.0.0.1']
 
 
 # Application definition
